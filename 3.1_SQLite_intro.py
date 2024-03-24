37549dbb7ee87b10216f8269c1b74ad8ab9b9b26
Walts1991 <walts1991@googlemail.com>
2024-03-24 01:46:57 +0000

    Module 3 base file creation and 3.1 notes

---
+++ 3.1_SQLite_intro.py
@@ -0,0 +1,13 @@
+#light version of SQL database
+#Includes main functions of SQL database e.g. insert, delete etc.
+#SQL database is a collection of tables
+#Tables contain columns / rows
+#Columns contain data types
+#Rows contain data records
+#Databases are designed to be used / accessed in small chunks by a machine
+#Files optimised to be used and viewed all at once by a user
+#Columns must be defined in advanced including data types
+#Can have problems if changing a table later on
+#50+ data types in SQL
+#SQLite has fewer options
+#Can also have a local MySQL database but can be a pain to set up
