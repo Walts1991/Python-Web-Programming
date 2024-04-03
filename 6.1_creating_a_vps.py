566f0c8e95392ba788a0efd4c099fc314d92f5c4
Walts1991 <walts1991@googlemail.com>
2024-04-04 00:57:09 +0100

    6.1 notes

---
+++ 6.1_creating_a_vps.py
@@ -0,0 +1,18 @@
+#
+'''
+VPS = Virtual Private Server
+Easier to expand / contract as required - External host from local
+AWS is the largest host - Complex set up as need to set specific parameters - Not suitable for beginners
+Linode - Cheaper the Digital Ocean for larger/faster system requirements
+DigitalOcean - Cheap and easy setup for basic requirements
+DigitalOcean account setup
+
+Google Account
+
+Create droplet - Select requirements (cheapest) / select location / Ubuntu operating system / Can add SSH key if required
+Backups highly recommended - additional 20% to monthly cost
+
+Example - Py-Web-Prog_ec4u
+*****C****
+
+'''
